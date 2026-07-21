f09e4f41eb90fcf9a3b75de59828f3926f6bebd1
Soeren Michaelis <soeren.michaelis@proton.me>
2026-07-21 08:54:37 +0200

    added main

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"timetrack/internal/adapters/cli"
+	web "timetrack/internal/adapters/http"
+	"timetrack/internal/adapters/sqlite"
+	"timetrack/internal/core/service"
+)
+
+func main() {
+	dbPath := os.Getenv("TIMETRACK_DB")
+	if dbPath == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Fehler:", err)
+			os.Exit(1)
+		}
+		dir := filepath.Join(home, ".timetrack")
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			fmt.Fprintln(os.Stderr, "Fehler:", err)
+			os.Exit(1)
+		}
+		dbPath = filepath.Join(dir, "timetrack.db")
+	}
+
+	sqlDB, err := sqlite.Open(dbPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Fehler:", err)
+		os.Exit(1)
+	}
+	defer sqlDB.Close()
+
+	svc := service.New(sqlite.NewRepo(sqlDB), time.Local)
+	app := &cli.App{
+		Svc:    svc,
+		Loc:    time.Local,
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Serve:  func(port int) error { return web.Serve(svc, time.Local, port) },
+	}
+	os.Exit(app.Run(os.Args[1:]))
+}
